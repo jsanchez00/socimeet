27b15b208460f6fb00ab9d5bdd90a9061108820e
jsanchez <jsanchez@uxland.es>
2022-05-08 10:50:31 +0200

    fix: port

--- server/server.ts
+++ server/server.ts
@@ -1,36 +1,34 @@
+import cors from 'cors';
+import express from 'express';
+import mongoose from 'mongoose';
+import path from 'path';
+import { router as userRoutes } from './routes/user';
+import './passport/local-auth';
 
-import cors from "cors";
-import express from "express";
-import mongoose from "mongoose";
-import path from "path";
-import { router as userRoutes } from "./routes/user";
-import "./passport/local-auth";
-
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || process.env.WEBSITES_PORT || 5000;
 
 const app = express();
 app.use(express.json());
 
 // Serve the React static files after build
-app.use(express.static("client/build"));
-app.use("/api", userRoutes);
+app.use(express.static('client/build'));
+app.use('/api', userRoutes);
 app.use(cors);
 
-mongoose.connect(
-  process.env.MONGODB_URI as string
-)
-.then(() => console.log("connected to mongodb"))
-.catch((e: Error) => console.error(e.message));
+mongoose
+  .connect(process.env.MONGODB_URI as string)
+  .then(() => console.log('connected to mongodb'))
+  .catch((e: Error) => console.error(e.message));
 
 app.listen(port, () => {
   console.log(`Server listening on ${port}`);
 });
 
-app.get("/api/hello", (req, res) => {
-  res.send({ message: "Hello" });
+app.get('/api/hello', (req, res) => {
+  res.send({ message: 'Hello' });
 });
 
 // All other unmatched requests will return the React app
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build",  "index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
