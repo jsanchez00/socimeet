cc3269a95d8b3fefaa926bf584c1c05f8a6ee85a
jsanchez <jsanchez@uxland.es>
2022-05-11 17:12:37 +0200

    fix: server

--- server/server.ts
+++ server/server.ts
@@ -5,30 +5,32 @@ import path from 'path';
 import { router as userRoutes } from './routes/user';
 import './passport/local-auth';
 
-const port = process.env.PORT || process.env.WEBSITES_PORT || 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
-
-// Serve the React static files after build
-app.use(express.static('client/build'));
 app.use('/api', userRoutes);
-app.use(cors);
+let MONGODB_URI = `mongodb://localhost:27017`;
+
+if (process.env.C_ENV === 'P') {
+  const mongoPassword = '12345';
+  const config = JSON.parse(process.env.APP_CONFIG);
+  MONGODB_URI = 'mongodb://' + config.mongo.user + ':' + encodeURIComponent(mongoPassword) + '@' + config.mongo.hostString;
+}
 
 mongoose
-  .connect(process.env.MONGODB_URI as string)
-  .then(() => console.log('connected to mongodb'))
+  .connect(MONGODB_URI as string)
+  .then(() => console.log(`connected to mongodb ${MONGODB_URI}`))
   .catch((e: Error) => console.error(e.message));
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// All other unmatched requests will return the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server listening on ${port}`);
-});
-
-app.get('/api/hello', (req, res) => {
-  res.send({ message: 'Hello' });
-});
-
-// All other unmatched requests will return the React app
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
