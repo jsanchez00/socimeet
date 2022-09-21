f8c6c03515c7b8f70effc46547ed5e90c219512d
Jordi Sanchez <jsanchez@uxland.es>
2022-09-21 17:07:59 +0200

    refactor(server): user info

--- client/src/app/application/commands/fetch-user-info.ts
+++ client/src/app/application/commands/fetch-user-info.ts
@@ -1,10 +1,9 @@
-import { IUserInfo } from '../../../interfaces/user';
-import { customFetch } from '../../infrastructure/fetch';
-import { updateUserAction } from './update-user-action';
+import { IUserInfo } from "../../../interfaces/user";
+import { customFetch } from "../../infrastructure/fetch";
+import { updateUserAction } from "./update-user-action";
 
 export const fetchUserInfo = (email: string): Promise<IUserInfo> =>
-  customFetch(`users/${email}`, { method: 'GET' }).then((r) => {
-    const userInfo = r[0];
+  customFetch(`users/${email}`, { method: "GET" }).then((userInfo) => {
     updateUserAction(userInfo);
     return userInfo;
   });
--- server/routes/user.ts
+++ server/routes/user.ts
@@ -1,20 +1,20 @@
-import path2 from 'path';
-import express from 'express';
-import { userModel } from '../models/user';
-import passport from 'passport';
-import multer from 'multer';
-import { Guid } from 'guid-typescript';
-import { promisify } from 'util';
-import fs from 'fs';
-import { without } from 'ramda';
+import path2 from "path";
+import express from "express";
+import { userModel } from "../models/user";
+import passport from "passport";
+import multer from "multer";
+import { Guid } from "guid-typescript";
+import { promisify } from "util";
+import fs from "fs";
+import { without } from "ramda";
 
-const DIR = './static/assets/avatars/';
+const DIR = "./static/assets/avatars/";
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, DIR);
   },
   filename: (req, file, cb) => {
-    const fileName = file.originalname.toLowerCase().split(' ').join('-');
+    const fileName = file.originalname.toLowerCase().split(" ").join("-");
     cb(null, `${Guid.create().toString()}-${fileName}`);
   },
 });
@@ -24,12 +24,16 @@ const upload = multer({
   limits: { fileSize: 8388608 }, // 8mb
   // limits: { fileSize: 2097152 }, // 2mb
   fileFilter: (req, file, cb) => {
-    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+    if (
+      file.mimetype === "image/png" ||
+      file.mimetype === "image/jpg" ||
+      file.mimetype === "image/jpeg"
+    ) {
       cb(null, true);
     } else {
       cb(null, false);
-      console.log('Only .png, .jpg and .jpeg format allowed!');
-      return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+      console.log("Only .png, .jpg and .jpeg format allowed!");
+      return cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
     }
   },
 });
@@ -38,21 +42,29 @@ const upload = multer({
 
 export const router = express.Router();
 
-const PATH = '/users';
+const PATH = "/users";
 
-router.post(`${PATH}/signup`, passport.authenticate('local-signup', { session: false }), (req: any, res, next) => {
-  res.json(req.user);
-});
+router.post(
+  `${PATH}/signup`,
+  passport.authenticate("local-signup", { session: false }),
+  (req: any, res, next) => {
+    res.json(req.user);
+  }
+);
 
-router.post(`${PATH}/login`, passport.authenticate('local-login', { session: false }), (req: any, res, next) => {
-  res.json(req.user);
-});
+router.post(
+  `${PATH}/login`,
+  passport.authenticate("local-login", { session: false }),
+  (req: any, res, next) => {
+    res.json(req.user);
+  }
+);
 
 router.get(`${PATH}/:email`, (req, res) => {
   const params = req.params;
   const email = params.email;
   userModel
-    .find({
+    .findOne({
       email,
     })
     .then((r) => res.json(r))
@@ -61,42 +73,52 @@ router.get(`${PATH}/:email`, (req, res) => {
 
 router.post(`${PATH}/update-user`, (req, res) => {
   userModel
-    .updateOne({ email: req.body.email }, without(['avatar'], req.body))
+    .updateOne({ email: req.body.email }, without(["avatar"], req.body))
     .then((r) => res.json(r))
     .catch((e: Error) => res.json({ messages: e.message }));
 });
 
-router.post(`${PATH}/update-avatar`, upload.single('profileImg'), async (req: any, res) => {
-  const userInfo = await userModel.findOne({ email: req.body.email });
-  if (userInfo.avatar) {
-    try {
-      let p = __dirname + '\\..\\..';
-      if (process.env.C_ENV === 'P') p = __dirname + '//..';
-      fs.realpath(p, (err, pathResolved: string) => {
-        if (err) {
-          console.log(err);
-        } else {
-          if (process.env.C_ENV === 'P') fs.unlinkSync(`${pathResolved}/static${userInfo.avatar}`);
-          else fs.unlinkSync(`${pathResolved}\\static${userInfo.avatar.split('/').join('\\')}`);
-        }
-      });
-    } catch (e) {
-      console.error(e);
-    }
-  }
-  const path = `/assets/avatars/${req.file.filename}`;
-  userModel
-    .updateOne(
-      {
-        email: req.body.email,
-      },
-      {
-        avatar: path,
+router.post(
+  `${PATH}/update-avatar`,
+  upload.single("profileImg"),
+  async (req: any, res) => {
+    const userInfo = await userModel.findOne({ email: req.body.email });
+    if (userInfo.avatar) {
+      try {
+        let p = __dirname + "\\..\\..";
+        if (process.env.C_ENV === "P") p = __dirname + "//..";
+        fs.realpath(p, (err, pathResolved: string) => {
+          if (err) {
+            console.log(err);
+          } else {
+            if (process.env.C_ENV === "P")
+              fs.unlinkSync(`${pathResolved}/static${userInfo.avatar}`);
+            else
+              fs.unlinkSync(
+                `${pathResolved}\\static${userInfo.avatar
+                  .split("/")
+                  .join("\\")}`
+              );
+          }
+        });
+      } catch (e) {
+        console.error(e);
       }
-    )
-    .then((r) => {
-      res.json({
-        avatar: path,
+    }
+    const path = `/assets/avatars/${req.file.filename}`;
+    userModel
+      .updateOne(
+        {
+          email: req.body.email,
+        },
+        {
+          avatar: path,
+        }
+      )
+      .then((r) => {
+        res.json({
+          avatar: path,
+        });
       });
-    });
-});
+  }
+);
